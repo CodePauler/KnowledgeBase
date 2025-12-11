0cf4aecd0b536e8b6eb126fb4e1196b1dfae6fa3
CodePaluer <2205036781@qq/com>
2025-12-11 22:04:40 +0800

    add Custom error controller to handle 404 errors

---
+++ Backend/src/main/java/com/knowledgebase/backend/Controller/CustomErrorController.java
@@ -0,0 +1,20 @@
+package com.knowledgebase.backend.Controller;
+
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public Map<String, Object> handleError() {
+        Map<String, Object> error = new HashMap<>();
+        error.put("code", 404);
+        error.put("message", "This path does not exist");
+        return error;
+    }
+}
